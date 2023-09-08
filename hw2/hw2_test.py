f158d3007ee586a191db0b54fc180ddaa1829b0a
Julia-Run <doracodenow@gmail.com>
2023-09-08 22:21:19 +0800

    hw2

---
+++ hw2/hw2_test.py
@@ -0,0 +1,28 @@
+from hw2_test import preprocess_data, LibriDataset, DataLoader, batch_size
+# load data
+test_X = preprocess_data(split='test', feat_dir='./libriphone/feat', phone_path='./libriphone', concat_nframes=concat_nframes)
+test_set = LibriDataset(test_X, None)
+test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
+# load model
+model = Classifier(input_dim=input_dim, hidden_layers=hidden_layers, hidden_dim=hidden_dim).to(device)
+model.load_state_dict(torch.load(model_path))
+
+test_acc = 0.0
+test_lengths = 0
+pred = np.array([], dtype=np.int32)
+
+model.eval()
+with torch.no_grad():
+    for i, batch in enumerate(tqdm(test_loader)):
+        features = batch
+        features = features.to(device)
+
+        outputs = model(features)
+
+        _, test_pred = torch.max(outputs, 1) # get the index of the class with the highest probability
+        pred = np.concatenate((pred, test_pred.cpu().numpy()), axis=0)
+
+with open('prediction.csv', 'w') as f:
+    f.write('Id,Class\n')
+    for i, y in enumerate(pred):
+        f.write('{},{}\n'.format(i, y))
